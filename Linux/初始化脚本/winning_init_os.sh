a30173fba16aa5f7508f1999935442f31e454faa
chensj <chenshijie1988@yeah.net>
2021-05-17 19:59:18 +0800

    add init

--- Linux/初始化脚本/winning_init_os.sh
+++ Linux/初始化脚本/winning_init_os.sh
@@ -196,7 +196,8 @@ echo '---' > user_info.txt
 for user in winning winsupport ftpuser; do
   if ! id $user > /dev/null 2>&1 ; then
     # 生成一个只用左手就能输入的8位密码
-    user_pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c8 ; echo)
+    #user_pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c8 ; echo)
+    user_pass='Win.2021'
     case $user in
       ftpuser) 
          useradd -d /home/vsftpd -s /usr/sbin/nologin $user 
@@ -225,7 +226,8 @@ for user in winning winsupport ftpuser; do
   else
     Echo_Green "$user 已经创建 重新修改密码 添加sudo权限"
     # 生成一个只用左手就能输入的8位密码
-    user_pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c8 ; echo) 
+    #user_pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c8 ; echo) 
+    user_pass='Win.2021'
     echo "$user":"$user_pass" | chpasswd
     echo "用户:$user 密码: $user_pass 密码修改完成" >> user_info.txt
     # 添加sudo权限
@@ -355,9 +357,35 @@ sys_conf_sysctl=/etc/sysctl.conf
 sys_kernel_config_exists=$(cat $sys_conf_sysctl | grep 'vm.max_map_count')
 if [ -z "$sys_kernel_config_exists" ]; then
 echo '
-net.core.somaxconn = 32768
 kernel.pid_max = 65536
-vm.max_map_count = 655300' >> $sys_conf_sysctl
+vm.max_map_count = 655300
+net.ipv4.tcp_keepalive_time = 1800
+net.ipv4.tcp_keepalive_probes = 3
+net.ipv4.tcp_keepalive_intvl = 15
+# 下面主要是内核参数的优化，包括tcp的快速释放和重利用等。
+net.ipv4.tcp_max_syn_backlog = 65536
+net.core.netdev_max_backlog =  32768
+net.core.somaxconn = 32768
+
+net.core.wmem_default = 8388608
+net.core.rmem_default = 8388608
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syn_retries = 2
+
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+
+net.ipv4.tcp_mem = 94500000 915000000 927000000
+net.ipv4.tcp_max_orphans = 3276800
+
+#net.ipv4.tcp_fin_timeout = 30
+#net.ipv4.tcp_keepalive_time = 120
+net.ipv4.ip_local_port_range = 50000  65535
+# 以上信息卸载nginx后删除' >> $sys_conf_sysctl
 #sysctl -p > /dev/null 2>&1
 sysctl -p
 fi
